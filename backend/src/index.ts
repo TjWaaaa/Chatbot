6afd3d4b1578a31f3b2be383f34331c17ce1260d
Jens Schlegel <js414@hdm-stuttgart.de>
2022-11-13 20:08:09 +0100

    add prettier and eslint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,24 @@
-import express, { Application, Request, Response } from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(cors({ origin: true }))
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: true }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Healthy')
-})
+	res.send('Healthy');
+});
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`)
-})
+	console.log(`Server is running on PORT ${PORT}`);
+});
