8f80a9b59840d971bc128fdc55d7dd6df5317527
Zimmermann Philipp <pz016@hdm-stuttgart.de>
2022-11-17 10:22:47 +0100

    implement routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
-
 function App() {
-  return (
-    <h1 className='text-3xl font-bold'>App.js</h1>
-  );
+  return <h1 className="text-3xl font-bold">App.js</h1>;
 }
 
 export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,5 @@
+function Index() {
+  return <h1 className="text-3xl font-bold">Login</h1>;
+}
+
+export default Index;
