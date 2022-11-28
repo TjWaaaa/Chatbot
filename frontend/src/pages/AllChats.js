34ab31a7cd9881912a0d99124d49afcdd2b309af
Zimmermann Philipp <pz016@hdm-stuttgart.de>
2022-11-28 13:18:27 +0100

    Feature/Add All Chats + Chat Detail View #17

--- frontend/src/pages/AllChats.js
+++ frontend/src/pages/AllChats.js
@@ -1,12 +1,16 @@
-import {Link} from "react-router-dom";
+import Chat from "../components/Chat";
+import NavigationAllChats from "../components/NavigationAllChats";
 
 function Index() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">All Chats</h1>
-      <Link to="/dashboard">
-        <button type="button">Click Me!</button>
-      </Link>
+      <NavigationAllChats />
+      <Chat
+        name="Business Mann 42"
+        img="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_700,h_700/https://www.corporatephotographerslondon.com/wp-content/uploads/2021/06/professional-LinkedIn-profile-photo-London-1.jpg"
+        text="Zeit ist Geld, komm zum Punkt!"
+        time="Gestern"
+      />
     </div>
   );
 }
