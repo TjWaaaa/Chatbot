8f964e5e201bbf6b7307b7baee706fa9d62a67cf
LetsXeri <marcel.d.willie@gmail.com>
2022-12-12 13:26:21 +0100

    typing animation integrated

---
+++ frontend/src/components/MessageBotTyping.js
@@ -0,0 +1,19 @@
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+function Index({ text }) {
+	return (
+		<div className="flex flex-row mt-4 items-center ml-4">
+			<img
+				src="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_700,h_700/https://www.corporatephotographerslondon.com/wp-content/uploads/2021/06/professional-LinkedIn-profile-photo-London-1.jpg"
+				alt="Business Mann 42"
+				className="h-8 w-8 rounded-full"
+			/>
+			<div className="pl-4 pr-4 pt-2 pb-2 ml-4 mr-4 bg-white rounded-lg drop-shadow-sm">
+				<Player autoplay loop src="/animations/chat-typing-indicator.json" style={{ height: "300px", width: "300px" }}>
+					<Controls visible={true} buttons={["play", "repeat", "frame", "debug"]} />
+				</Player>
+			</div>
+		</div>
+	);
+}
+
+export default Index;
--- frontend/src/pages/SingleChat.js
+++ frontend/src/pages/SingleChat.js
@@ -1,56 +1,64 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-import NavigationChat from '../components/NavigationChat';
-import MessageBot from '../components/MessageBot';
-import MessageUser from '../components/MessageUser';
-import InputChat from '../components/InputChat';
-import { socket } from '..';
+import NavigationChat from "../components/NavigationChat";
+import MessageBot from "../components/MessageBot";
+import MessageBotTyping from "../components/MessageBotTyping";
+import MessageUser from "../components/MessageUser";
+import InputChat from "../components/InputChat";
+import { socket } from "..";
 
 function Index() {
-  const messagesEndRef = useRef(null);
+	const [showTypingAnimation, setShowTypingAnimation] = useState(false);
+	const messagesEndRef = useRef(null);
 
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
+	const scrollToBottom = () => {
+		messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+	};
 
-  const handleSendMessage = (message) => {
-    scrollToBottom();
-    socket.emit('message', message, 'businessMan');
-  };
+	const handleSendMessage = (message) => {
+		setShowTypingAnimation(true);
+		setTimeout(() => {
+			scrollToBottom();
+		}, 200);
+		socket.emit("message", message, "businessMan");
+	};
 
-  socket.on('answer', (message) => {
-    console.log(message);
-  });
+	socket.on("answer", (message) => {
+		setShowTypingAnimation(false);
+		scrollToBottom();
+		console.log(message);
+	});
 
-  useEffect(() => {
-    scrollToBottom();
-  }, []);
+	useEffect(() => {
+		scrollToBottom();
+	}, []);
 
-  return (
-    <div className="bg-slate-100 min-h-screen pb-24">
-      <NavigationChat
-        name="Business Mann 42"
-        img="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_700,h_700/https://www.corporatephotographerslondon.com/wp-content/uploads/2021/06/professional-LinkedIn-profile-photo-London-1.jpg"
-      />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageUser text="Sanctus est Lorem ipsum dolor sit amet" />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageUser text="Lorem ipsum dolor" />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
-      <InputChat sendMessage={handleSendMessage} />
-      <div ref={messagesEndRef} />
-    </div>
-  );
+	return (
+		<div className="bg-slate-100 min-h-screen pb-24">
+			<NavigationChat
+				name="Business Mann 42"
+				img="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_700,h_700/https://www.corporatephotographerslondon.com/wp-content/uploads/2021/06/professional-LinkedIn-profile-photo-London-1.jpg"
+			/>
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageUser text="Sanctus est Lorem ipsum dolor sit amet" />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageUser text="Lorem ipsum dolor" />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			<MessageBot text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " />
+			{showTypingAnimation ? <MessageBotTyping /> : <></>}
+			<InputChat sendMessage={handleSendMessage} />
+			<div ref={messagesEndRef} />
+		</div>
+	);
 }
 
 export default Index;
