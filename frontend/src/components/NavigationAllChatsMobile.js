465c9245488e5f2894472b699c6d3bde3ad773d8
Zimmermann Philipp <pz016@hdm-stuttgart.de>
2023-01-09 11:25:31 +0100

    Feature/link settings and optimize chatbot #37

--- frontend/src/components/NavigationAllChatsMobile.js
+++ frontend/src/components/NavigationAllChatsMobile.js
@@ -3,7 +3,7 @@ import { Cog6ToothIcon } from '@heroicons/react/24/solid';
 
 function Index() {
 	return (
-		<div className="pl-4 pr-4 pt-8 pb-4 bg-white sticky top-0 z-50">
+		<div className="pl-4 pr-4 pt-4 pb-4 bg-white sticky top-0 z-50">
 			<div className="flex flex-row justify-between items-center">
 				<h1 className="text-3xl font-bold">All Chats</h1>
 				<Link to="/settings">
