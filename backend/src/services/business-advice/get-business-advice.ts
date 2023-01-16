7603a71d4051287134f68422eda56f9f958f43b3
Marvin <87226274+mlp909@users.noreply.github.com>
2023-01-16 14:27:33 +0100

    changed chatnames

--- backend/src/services/business-advice/get-business-advice.ts
+++ backend/src/services/business-advice/get-business-advice.ts
@@ -1,7 +1,17 @@
 import logger from '../../utils/logger';
 
 export const getBusinessAdvice = () => {
-	const advices = ['Ich hab keine Lust mir dir zu reden. Du FISCH!', 'Lass mich in Ruhe!', 'Halt die Fresse! Ich will schlafen', 'Weißt du', 'Ich würde mich ja geistig mit dir duellieren, aber wie ich sehe, bist du unbewaffnet.', 'Ich würde jetzt nicht sagen, dass du dumm bist, aber ich könnt’s aufschreiben, wenn du magst.','Hör auf mir zu schreiben und geh auf die Weide zu den anderen Kühen!', 'Ich bin grade auf dem Klo, lass mich jetzt scheissen und verpiss dich!', 'Du hast schon keine Hobbies wenn du mir die ganze Zeit schreibst oder?!'];
+	const advices = [
+		'Ich hab keine Lust mir dir zu reden. Du FISCH!',
+		'Lass mich in Ruhe!',
+		'Halt die Fresse! Ich will schlafen',
+		'Weißt du wie nervig du eigentlich bist?',
+		'Ich würde mich ja geistig mit dir duellieren, aber wie ich sehe, bist du unbewaffnet.',
+		'Ich würde jetzt nicht sagen, dass du dumm bist, aber ich könnt’s aufschreiben, wenn du magst.',
+		'Hör auf mir zu schreiben und geh auf die Weide zu den anderen Kühen!',
+		'Ich bin grade auf dem Klo, lass mich jetzt scheissen und verpiss dich!',
+		'Du hast schon keine Hobbies wenn du mir die ganze Zeit schreibst oder?!',
+	];
 
 	const answer = advices[Math.floor(Math.random() * advices.length)];
 	logger.info(`Business advice: ${answer}`);
