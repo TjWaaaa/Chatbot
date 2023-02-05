730f38216b10b809e05a31751139087ec816cd44
Jens Schlegel <jens@bitfactory.io>
2023-02-05 20:16:07 +0100

    remove duplicate code service user

--- backend/src/services/user/index.ts
+++ backend/src/services/user/index.ts
@@ -1,28 +1,5 @@
-import { User } from '@prisma/client';
-import { Context } from '../../configs/prisma';
-import { USER_DOES_NOT_EXIST } from '../../consts/error-messages';
-import userDb from '../db/user';
+import { deleteUser } from './delete-user';
+import { getUser } from './get-user';
+import { updateUserEmail } from './update-user';
 
-const updateUserEmail = async (userId: string, newEmail: string, ctx: Context): Promise<User> => {
-	await getUser(userId, ctx);
-
-	return await userDb.updateUserEmailById(userId, newEmail, ctx);
-};
-
-async function getUser(userId: string, ctx: Context): Promise<User> {
-	const user = await userDb.getUserById(userId, ctx);
-
-	if (!user) {
-		throw new Error(USER_DOES_NOT_EXIST);
-	}
-
-	return user;
-}
-
-const deleteUser = async (userId: string, ctx: Context): Promise<User> => {
-	await getUser(userId, ctx);
-
-	return userDb.deleteUserById(userId, ctx);
-};
-
-export default { deleteUser, updateUserEmail, getUser };
+export { deleteUser, updateUserEmail, getUser };
